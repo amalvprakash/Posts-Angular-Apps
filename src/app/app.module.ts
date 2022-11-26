c61acc2735a506c2bf49f96feea26017819e9593
Amal <amalprakash2025@gmail.com>
2022-11-27 01:29:12 +0530

    added add posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPostsComponent } from './add-posts/add-posts.component';
+import { RouterModule, Routes } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddPostsComponent
+  }
+  ]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
